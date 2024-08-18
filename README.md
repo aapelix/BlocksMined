f4d84f3f9debf32a10cf6059df91f66c790100d4
aapelix <mcaapelix@gmail.com>
2024-08-18 17:39:45 +0300

    a

--- README.md
+++ README.md
@@ -1,24 +1,45 @@
-### A Fabric 1.21 server-side mod that tracks every mined block and updates a scoreboard with the player's score based on their mining activity.
+## A Fabric 1.21 server-side mod that tracks every mined block and updates a scoreboard with the player's score based on their mining activity.
 
 ![Preview of the mod in game](https://cdn.modrinth.com/data/cached_images/bebcb15641e9386ea4fe7cd20f8de0985fe3a719.png)
 
-The name of the scoreboard is ´blocksMined´
+Any feedback/improvement ideas/bugs can be sent to `hello@aapelix.dev` or you can create an issue of it on Github
 
-To display the scoreboard on sidebar use the following command:
+### To set the display location for the scoreboard:
 ```
-/scoreboard objectives setdisplay sidebar blocksMined
+/bmdisplay <DISPLAY POSITION>
 ```
 
-If you want you can change the `sidebar` to like `list` or `below_name` to display the scoreboard somewhere else
+<DISPLAY POSITION> = THe position that you want to display the scoreboard in, like sidebar, belowname, list
 
-To modify the scoreboard (I mean like clear scores, add scores, change display name) you can just use Minecraft's own built in commands like:
 
+### Changing the color of the scores in the scoreboard:
+```
+/bmcolor <COLOR HERE>
+```
+
+Currently the **only supported colors are red and yellow**
+
+
+### To modify the scoreboard scores:
 
 ```
-/scoreboard players add aapelix blocksMined 100
-/scoreboard players reset @a blocksMined
+/bmaddscore aapelix 100
+/bmremovescore aapelix 100
 ```
+
+to reset all scores:
+
+```
+/bmreset
+```
+
+(you can use minecraft's built in commands to do stuff with the scoreboard but these commands are much more simplier)
+
+# Happy mining!
 
 ### Why did I make this?
 
 I've been looking for a mod to track these things on the SMP server that my friends and I play on, but everything I found was outdated or not working, so I decided to make one myself.
+
+### New features coming soonTM:
+- amogus
--- src/main/java/blocksmined/BlocksMined.java
+++ src/main/java/blocksmined/BlocksMined.java
@@ -1,10 +1,15 @@
 package blocksmined;
 
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.command.argument.ColorArgumentType;
+import net.minecraft.command.argument.ScoreHolderArgumentType;
+import net.minecraft.command.argument.ScoreboardObjectiveArgumentType;
 import net.minecraft.scoreboard.*;
 import net.minecraft.scoreboard.number.StyledNumberFormat;
 import net.minecraft.text.Text;
@@ -23,47 +28,155 @@ public class BlocksMined implements ModInitializer {
 
 		PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, entity) -> {
 			if (player.getServer() != null)
-				addScore(player.getServer(), (ServerPlayerEntity) player, "blocksMined");
+				addScore(player.getServer(), (ServerPlayerEntity) player);
 		});
 
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->  dispatcher.register(literal("blocksmined")
-				.then(argument("color", ColorArgumentType.color())
-						.executes(context -> {
-							final Formatting value = ColorArgumentType.getColor(context, "color");
-							final String result = value.asString();
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+					dispatcher.register(literal("bmcolor")
+							.then(argument("color", ColorArgumentType.color())
+									.executes(context -> {
+										final Formatting value = ColorArgumentType.getColor(context, "color");
+										final String result = value.asString();
 
-							Scoreboard scoreboard = context.getSource().getServer().getScoreboard();
+										Scoreboard scoreboard = context.getSource().getServer().getScoreboard();
+
+										if (Objects.equals(result, "red")) {
+											Objects.requireNonNull(scoreboard.getNullableObjective("blocksMined")).setNumberFormat(StyledNumberFormat.RED);
+											context.getSource().sendFeedback(() -> Text.literal("Changed scoreboard color to red").formatted(Formatting.RED), true);
+										} else if (Objects.equals(result, "yellow")) {
+											Objects.requireNonNull(scoreboard.getNullableObjective("blocksMined")).setNumberFormat(StyledNumberFormat.YELLOW);
+											context.getSource().sendFeedback(() -> Text.literal("Changed scoreboard color to red").formatted(Formatting.YELLOW), true);
+										} else {
+											context.getSource().sendFeedback(() -> Text.literal("Currently only yellow and red are supported").formatted(Formatting.RED, Formatting.BOLD), false);
+										}
+
+										return 1;
+									}))
+					);
+					dispatcher.register(literal("bmreset")
+							.requires(source -> source.hasPermissionLevel(2))
+									.executes(context -> {
+
+										Scoreboard scoreboard = context.getSource().getServer().getScoreboard();
+										ScoreboardObjective objective = scoreboard.getNullableObjective("blocksMined");
+
+										if (objective == null) {
+											context.getSource().sendFeedback(() -> Text.literal("Scoreboard doesn't exits!").formatted(Formatting.RED), false);
+											return 1;
+										}
 
 
-							if (Objects.equals(result, "red")) {
-								Objects.requireNonNull(scoreboard.getNullableObjective("blocksMined")).setNumberFormat(StyledNumberFormat.RED);
-								context.getSource().sendFeedback(() -> Text.literal("Changed scoreboard color to red").formatted(Formatting.RED), true);
-							}
+											scoreboard.removeObjective(objective);
+											scoreboard.addObjective(
+													"blocksMined",
+													ScoreboardCriterion.DUMMY,
+													Text.literal("Blocks Mined"),
+													ScoreboardCriterion.RenderType.INTEGER,
+													true,
+													StyledNumberFormat.RED);
 
-							else if (Objects.equals(result, "yellow")) {
-								Objects.requireNonNull(scoreboard.getNullableObjective("blocksMined")).setNumberFormat(StyledNumberFormat.YELLOW);
-								context.getSource().sendFeedback(() -> Text.literal("Changed scoreboard color to red").formatted(Formatting.YELLOW), true);
-							} else {
-								context.getSource().sendFeedback(() -> Text.literal("Currently only yellow and red are supported").formatted(Formatting.RED, Formatting.BOLD), false);
-							}
 
-							return 1;
-						}))));
+
+
+										return 1;
+					}));
+					dispatcher.register(literal("bmdisplay")
+							.requires(source -> source.hasPermissionLevel(2))
+							.then(argument("position", StringArgumentType.string())
+									.executes(context -> {
+										final String value = StringArgumentType.getString(context, "position");
+
+										Scoreboard scoreboard = context.getSource().getServer().getScoreboard();
+										ScoreboardObjective objective = scoreboard.getNullableObjective("blocksMined");
+
+										if (objective == null) {
+											context.getSource().sendFeedback(() -> Text.literal("Scoreboard doesn't exits!").formatted(Formatting.RED), false);
+											return 1;
+										}
+
+										if (Objects.equals(value, "sidebar")) {
+											scoreboard.setObjectiveSlot(ScoreboardDisplaySlot.SIDEBAR, objective);
+										} else if (Objects.equals(value, "list")) {
+											scoreboard.setObjectiveSlot(ScoreboardDisplaySlot.LIST, objective);
+										} else if (Objects.equals(value, "belowName") || Objects.equals(value, "belowname")) {
+											scoreboard.setObjectiveSlot(ScoreboardDisplaySlot.BELOW_NAME, objective);
+										}
+
+										return 1;
+									}))
+					);
+					dispatcher.register(literal("bmaddscore")
+							.requires(source -> source.hasPermissionLevel(2))
+							.then(argument("player", ScoreHolderArgumentType.scoreHolder())
+									.then(argument("amount", IntegerArgumentType.integer())
+										.executes(context -> {
+											final ScoreHolder player = ScoreHolderArgumentType.getScoreHolder(context, "player");
+											final int amount = IntegerArgumentType.getInteger(context, "amount");
+
+											Scoreboard scoreboard = context.getSource().getServer().getScoreboard();
+											ScoreboardObjective objective = scoreboard.getNullableObjective("blocksMined");
+
+											if (objective == null) {
+												context.getSource().sendFeedback(() -> Text.literal("Scoreboard doesn't exits!").formatted(Formatting.RED), false);
+												return 1;
+											}
+
+											ScoreAccess blocksMinedScore = scoreboard.getOrCreateScore(player, objective, true);
+											ReadableScoreboardScore currentScore = scoreboard.getScore(ScoreHolder.fromName(player.getNameForScoreboard()), objective);
+
+											if (currentScore != null)
+												blocksMinedScore.setScore(currentScore.getScore() + amount);
+											else
+												blocksMinedScore.setScore(amount);
+
+											return 1;
+										})))
+					);
+					dispatcher.register(literal("bmremovescore")
+						.requires(source -> source.hasPermissionLevel(2))
+						.then(argument("player", ScoreHolderArgumentType.scoreHolder())
+							.then(argument("amount", IntegerArgumentType.integer())
+								.executes(context -> {
+									final ScoreHolder player = ScoreHolderArgumentType.getScoreHolder(context, "player");
+									final int amount = IntegerArgumentType.getInteger(context, "amount");
+
+									Scoreboard scoreboard = context.getSource().getServer().getScoreboard();
+									ScoreboardObjective objective = scoreboard.getNullableObjective("blocksMined");
+
+									if (objective == null) {
+										context.getSource().sendFeedback(() -> Text.literal("Scoreboard doesn't exits!").formatted(Formatting.RED), false);
+										return 1;
+									}
+
+									ScoreAccess blocksMinedScore = scoreboard.getOrCreateScore(player, objective, true);
+									ReadableScoreboardScore currentScore = scoreboard.getScore(ScoreHolder.fromName(player.getNameForScoreboard()), objective);
+
+									if (currentScore != null)
+										blocksMinedScore.setScore(currentScore.getScore() - amount);
+									else
+										context.getSource().sendFeedback(() -> Text.literal("Score can't be below zero").formatted(Formatting.RED), false);
+
+									return 1;
+								})))
+			);
+			}
+		);
 	}
 
-	public static void addScore(MinecraftServer server, ServerPlayerEntity player, String objectiveName) {
+	public static void addScore(MinecraftServer server, ServerPlayerEntity player) {
 		Scoreboard scoreboard = server.getScoreboard();
-		ScoreboardObjective objective = scoreboard.getNullableObjective(objectiveName);
+		ScoreboardObjective objective = scoreboard.getNullableObjective("blocksMined");
 
 		if (objective == null) {
 			objective = scoreboard.addObjective(
-					objectiveName,
+					"blocksMined",
 					ScoreboardCriterion.DUMMY,
 					Text.literal("Blocks Mined"),
 					ScoreboardCriterion.RenderType.INTEGER,
 					true,
 					StyledNumberFormat.RED
 			);
+			scoreboard.setObjectiveSlot(ScoreboardDisplaySlot.SIDEBAR, objective);
 		}
 
 		ScoreAccess blocksMinedScore = scoreboard.getOrCreateScore(player, objective, true);
